e1dd5f9b39ec79b841340d9bde0cda62622dd520
yuvalweiz <106019626+yuvalweiz@users.noreply.github.com>
2022-05-28 17:58:10 +0300

    After algorithmes debag

--- src/test/GeneralCheckingFunctions.java
+++ src/test/GeneralCheckingFunctions.java
@@ -12,7 +12,8 @@ public class GeneralCheckingFunctions {
 
     public static String getGithubLink(){
 //change the <username> in the link to the username of the student who   created the github project:
-        String githubLink = "https://github.com/<username>/ATP-Project";
+        String githubLink = "https://github.com/yuvalweiz/ATP-Project-PartA";
+
         return githubLink;
     }
 
